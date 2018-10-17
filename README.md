5a679222f6eeaaf11769f3d11e713bd48ecd1a58
Gabe DeFreitas <defreitas@dyn-209-2-225-252.dyn.columbia.edu>
2018-10-17 17:01:00 -0400

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bgslide
-Background photo slideshow script for macOS written in Go
+A Go script for customizing a background slideshow for macOS.
+
+## To Use
+
+1. Install package main: cd into repository and run `go build; go install`.
+2. Place desired background photos into 1 folder.
+3. Run following command `go run bgslide -dir <folder-path>`.
+
+## Dependencies
+
+* Full Go installation
--- src/build.go
+++ src/build.go
@@ -1,11 +1,13 @@
 package src
 
-func Build(d string , f string) string {
+// Build an AppleScript to call a photo from dir. Should also work across multiple
+// desktops.
+func Build(d string, f string) string {
 
 	str := "tell application \"System Events\"\n"
 	str += "set theDesktops to a reference to every desktop\n"
 	str += "repeat with x from 1 to (count theDesktops)\n"
-	str += "set picture of item x of the theDesktops to " + dfCombine(d,f) + "\n"
+	str += "set picture of item x of the theDesktops to " + dfCombine(d, f) + "\n"
 	str += "end repeat\n"
 	str += "end tell\n"
 
@@ -13,8 +15,8 @@ func Build(d string , f string) string {
 
 }
 
-func dfCombine( d string , f string ) string {
+func dfCombine(d string, f string) string {
 
-  return "\"" + d + "/" + f + "\""
+	return "\"" + d + "/" + f + "\""
 
 }
