fc64599ad59123ccc91b8a9691bc7c21975bea9b
Gabe DeFreitas <defreitas@Gabes-MacBook-Air.local>
2018-10-28 14:17:29 -0400

    Now passes string directly to applescript

--- bgslide.go
+++ bgslide.go
@@ -1,55 +1,44 @@
 package main
 
 import (
-
-  "io/ioutil"
-  "math/rand"
-  "os/exec"
-  "time"
-  "flag"
-  "bgslide/src"
-
+	"bgslide/src"
+	"flag"
+	"math/rand"
+	"os/exec"
+	"time"
 )
 
 var (
-
-  photoList []string
-  dir string
-
+	photoList []string
+	dir       string
 )
 
 const FLAG_HELP string = "Your Photo Directory"
 
 func main() {
 
-  rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-  flag.StringVar(&dir, "dir", src.GetDefaultDir() , FLAG_HELP)
-  flag.Parse()
+	flag.StringVar(&dir, "dir", src.GetDefaultDir(), FLAG_HELP)
+	flag.Parse()
 
-  // Change wallpaper when program starts (helps debug)
-  oneCycle()
+	// Change wallpaper when program starts (helps debug)
+	oneCycle()
 
-  // Changes wallpaper every 60 seconds
-  for range time.NewTicker( time.Second * 60 ).C {
+	// Changes wallpaper every 60 seconds
+	for range time.NewTicker(time.Second * 60).C {
 
-    oneCycle()
+		oneCycle()
 
-  }
+	}
 
 }
 
 func oneCycle() {
 
-  photoList = src.GetNames( dir )
+	photoList = src.GetNames(dir)
+	x := rand.Intn(len(photoList))
 
-  x := rand.Intn( len( photoList ) )
-
-  slc := []byte( src.Build( dir , photoList[x] ) )
-
-  ioutil.WriteFile( "./script" , slc  , 0755 )
-
-  exec.Command("osascript", "./script").Run()
-  exec.Command("rm", "./script").Run()
+	exec.Command("osascript", "-e", src.Build(dir, photoList[x])).Run()
 
 }
