83f3aeb5fdb099ea6d486a03e44a23ee106eaf45
Gabe DeFreitas <defreitas@dyn-209-2-224-239.dyn.columbia.edu>
2018-10-29 11:24:34 -0400

    Adding single photo capabilities

--- src/fileNames.go
+++ src/fileNames.go
@@ -5,12 +5,29 @@ import "os"
 // Make a slice containing the names of each file in dir
 func GetNames(dir string) []string {
 
-	f, err := os.Open(dir)
-	Check(err)
+  if ( checkIsDir( dir ) ) {
 
-	list, err := f.Readdirnames(0)
-	Check(err)
+	  f, err := os.Open(dir)
+	  Check(err)
 
-	return list
+	  list, err := f.Readdirnames(0)
+	  Check(err)
+
+	  return list
+
+  } else {
+
+    return []string{dir}
+
+  }
+
+}
+
+func checkIsDir( dir string ) bool {
+
+  f , err := os.Stat( dir )
+  Check(err)
+
+  return f.IsDir()
 
 }
