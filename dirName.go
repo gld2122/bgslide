fc69f3c95cd950d8bef2e568e735f9e52536c5c3
Gabe DeFreitas <gld2122@columbia.edu>
2019-03-24 00:53:29 -0400

    Simplified default directory maker

--- dirName.go
+++ dirName.go
@@ -2,23 +2,12 @@ package main
 
 import "os/user"
 
+// Will look for pictures by default in ~/Pictures/bgslide
 func getDefaultDir() string {
-
-	return addDir(getHome())
-
-}
-
-func getHome() string {
 
 	usr, err := user.Current()
 	Check(err)
 
-	return string(usr.HomeDir)
-
-}
-
-func addDir(s string) string {
-
-	return s + "/Pictures/bgslide"
+	return string(usr.HomeDir) + "/Pictures/bgslide"
 
 }
