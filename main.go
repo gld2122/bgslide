e116166e6b9266e4153797f1ead3e355e4ec54f0
Gabe DeFreitas <gld2122@columbia.edu>
2019-05-15 19:59:49 -0400

    Simplify program structure, del fileNames.go

--- main.go
+++ main.go
@@ -3,56 +3,59 @@ package main
 import (
 	"flag"
 	"math/rand"
+	"os"
 	"os/exec"
 	"time"
 )
 
-var (
-	photoList []string
-	dir       string
-)
-
-const FLAG_HELP string = "Your Photo Directory"
-
 func main() {
 
-	rand.Seed(time.Now().UnixNano())
+	var dir string
+	const FLAG_HELP string = "Your Photo Directory"
 
 	flag.StringVar(&dir, "dir", getDefaultDir(), FLAG_HELP)
 	flag.Parse()
 
-	if CheckIsDir(dir) {
+	rand.Seed(time.Now().UnixNano())
 
-		// Change wallpaper when program starts (helps debug)
-		oneCycle()
+	if checkDir(dir) {
 
 		// Changes wallpaper every 60 seconds
 		for range time.NewTicker(time.Second * 60).C {
 
-			oneCycle()
+			fp, err := os.Open(dir)
+			Check(err)
+
+			dirList, err := fp.Readdirnames(0)
+			Check(err)
+
+			x := rand.Intn(len(dirList))
+
+			// AppleScript to change wallpaper
+			exec.Command("osascript", "-e", BuildDir(dir, dirList[x])).Run()
 
 		}
 
 	} else {
 
-		changeToPhoto()
+		exec.Command("osascript", "-e", BuildFile(dir)).Run()
 
 	}
 
 }
 
-func oneCycle() {
+func checkDir(dir string) bool {
 
-	photoList = GetNames(dir)
-	x := rand.Intn(len(photoList))
+	fp, err := os.Open(dir)
+	Check(err)
 
-	// AppleScript to change wallpaper
-	exec.Command("osascript", "-e", BuildDir(dir, photoList[x])).Run()
+	st, err := fp.Stat()
+	Check(err)
 
-}
+	result := st.IsDir()
 
-func changeToPhoto() {
+	Check(fp.Close())
 
-	exec.Command("osascript", "-e", BuildFile(dir)).Run()
+	return result
 
 }
